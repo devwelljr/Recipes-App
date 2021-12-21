14cda6b25e80abd71bd7994925bc6c4858fae799
Hugoe-Emannuel <85767023+Hugoe-Emannuel@users.noreply.github.com>
2021-12-21 14:38:11 -0300

    add routes in the application

---
+++ src/pages/ExplorarBebidasComidas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExplorarBebidasComidas() {
+  return (
+    <h1> ExplorarBebidasComidas </h1>
+  );
+}
+
+export default ExplorarBebidasComidas;
---
+++ src/pages/ReceitaEmProgresso.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ReceitasEmProgresso() {
+  return (
+    <h1> ReceitasEmProgresso </h1>
+  );
+}
+
+export default ReceitasEmProgresso;
